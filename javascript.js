f865e56a1a4ead150bab6927e5b87cd5e9bc4b7f
mhmdwfa <190647188+mhmdwfa@users.noreply.github.com>
2025-01-12 16:46:14 +0300

    refactored to a DOM ui insted of console.log.

--- javascript.js
+++ javascript.js
@@ -1,6 +1,32 @@
-console.log("hi");
-let humanChoice;
-let computerChoice;
+let computerChoice = '';
+let computerSelection = getCumputerChoice;
+let humanChoice = '';
+let buttonPaper = document.createElement('button');
+buttonPaper.name = 'Paper';
+let buttonScissors = document.createElement('button');
+buttonScissors.name = 'Scissors';
+let buttonRock = document.createElement('button');
+buttonRock.name = 'Rock';
+let choices = document.createElement('h3');
+let score = document.createElement('h3');
+let div = document.createElement('div');
+let finalResult = document.createElement('h3');
+
+choices.innerText = 'human: ' + humanChoice + 'computer: ' + computerChoice;
+
+let body = document.body;
+body.append(div);
+div.append(buttonPaper);
+div.append(buttonRock);
+div.append(buttonScissors);
+div.append(choices);
+div.append(score);
+div.append(finalResult);
+console.log('hi');
+document.querySelectorAll('button').forEach((e) => {
+  console.log(e);
+  e.innerHTML = e.name;
+});
 
 function getRandom() {
   return Math.floor(Math.random() * 3) + 1;
@@ -9,39 +35,41 @@ function getRandom() {
 function getCumputerChoice() {
   getRandom();
   if (getRandom() === 1) {
-    computerChoice = "rock";
+    computerChoice = 'rock';
   } else if (getRandom() === 2) {
-    computerChoice = "paper";
+    computerChoice = 'paper';
   } else {
-    computerChoice = "scissors";
+    computerChoice = 'scissors';
   }
 }
-function getHumanChoice() {
-  let Human = prompt("Chose Rock, Paper ,Scissors");
-  Human = Human.toLowerCase();
-  while (Human !== "rock" && Human !== "paper" && Human !== "scissors") {
-    Human = prompt("check the spelling, rock, paper, scissors");
-    console.log(Human);
-  }
-  humanChoice = Human;
-}
+// function getHumanChoice() {
+//   let Human = prompt('Chose Rock, Paper ,Scissors');
+//   Human = Human.toLowerCase();
+//   while (Human !== 'rock' && Human !== 'paper' && Human !== 'scissors') {
+//     Human = prompt('check the spelling, rock, paper, scissors');
+//     console.log(Human);
+//   }
+//   humanChoice = Human;
+// }
 let humanScore = 0;
 let computerScore = 0;
 
-function playRound() {
+function playRound(humanChoice) {
   getCumputerChoice();
-  getHumanChoice();
+  choices.innerText =
+    'human: ' + humanChoice + '|| computer: ' + computerChoice;
   console.log(humanChoice);
   console.log(computerChoice);
+
   console.log(humanScore, computerScore);
   if (humanChoice === computerChoice) {
     console.log("It's a tie!");
     humanScore = ++humanScore;
     computerScore = ++computerScore;
   } else if (
-    (humanChoice === "rock" && computerChoice === "scissors") ||
-    (humanChoice === "scissors" && computerChoice === "paper") ||
-    (humanChoice === "paper" && computerChoice === "rock")
+    (humanChoice === 'rock' && computerChoice === 'scissors') ||
+    (humanChoice === 'scissors' && computerChoice === 'paper') ||
+    (humanChoice === 'paper' && computerChoice === 'rock')
   ) {
     humanScore = ++humanScore;
     console.log(`you won ${humanChoice} beats ${computerChoice}`);
@@ -49,22 +77,53 @@ function playRound() {
     computerScore = ++computerScore;
     console.log(`you lost ${computerChoice} beats ${humanChoice}`);
   }
-}
-// let humanSelection = getHumanChoice;
-// let computerSelection = getCumputerChoice;
-
-function playGame() {
-  while (humanScore < 5 && computerScore < 5) {
-    playRound();
-  }
+  score.innerText =
+    'your score is ' + humanScore + ', computer score is ' + computerScore;
   if (humanScore === 5 && computerScore === 5) {
     console.log("it's a draw!");
+    humanScore = 0;
+    computerScore = 0;
+    finalResult.innerText = "it's a draw!";
   } else if (humanScore === 5) {
-    console.log("You won!");
-  } else {
-    console.log("You lost!");
+    console.log('You won!');
+    humanScore = 0;
+    computerScore = 0;
+    finalResult.innerText = 'You won!';
+  } else if (computerScore === 5) {
+    console.log('You lost!');
+    humanScore = 0;
+    computerScore = 0;
+    finalResult.innerText = 'You lost!';
   }
-  console.log(humanScore, computerScore);
 }
-playGame();
-console.log();
+
+buttonPaper.addEventListener('click', () => {
+  playRound('paper');
+});
+buttonRock.addEventListener('click', () => {
+  playRound('rock');
+});
+buttonScissors.addEventListener('click', () => {
+  playRound('scissors');
+});
+
+// function playGame() {
+//   while (humanScore < 5 && computerScore < 5) {
+//     playRound();
+//   }
+// if (humanScore === 5 && computerScore === 5) {
+//   console.log("it's a draw!");
+//   humanScore = 0;
+//   computerScore = 0;
+//   finalResult.innerText = "it's a draw!";
+// } else if (humanScore === 5) {
+//   console.log('You won!');
+//   humanScore = 0;
+//   computerScore = 0;
+//   finalResult.innerText = 'You won!';
+// } else {
+//   console.log('You lost!');
+//   humanScore = 0;
+//   computerScore = 0;
+//   finalResult.innerText = 'You lost!';
+// }
